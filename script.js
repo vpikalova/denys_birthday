fbd9991aa19b6d847954c361bcdb75058c20aa1a
Valentyna Pikalova <vpikalova@hotmail.com>
2021-12-17 03:31:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
 
 function my0() {
-alert("Дорогой Денис! Пусть твоя жизнь всегда украшается прекрасными вещами: интересной работой, верными друзьями, музыкой, неожиданными встречами и невероятными путешествиями! А впечатления пусть остаются в памяти, в сердце и в фотографиях!")
+alert("Дорогой Денис - этот небольшой поздравительный квест для тебя! Кликай по драгоценным камням и отгадывай, кто же автор послания!")
+}
+function my1() {
+    alert("I wish you a long and amazing life. I wish you great health and excellent results in all your dealings. Most importantly, I wish you happiness without measure every minute in your life. Be loved, be happy, enjoy every day!")
 }
 
 function my2() {
     alert("Желаю увидеть, услышать, постичь и испытать всё то, о чем душа мечтает")
+}
+
+
+function my3() {
+    alert("Дорогой Денис! Пусть твоя жизнь всегда украшается прекрасными вещами: интересной работой, верными друзьями, музыкой, неожиданными встречами и невероятными путешествиями! А впечатления пусть остаются в памяти, в сердце и в фотографиях!")
+}
+function my4() {
+    alert("Денечка, хочу тебе пожелание в День Рождения выразить вот такой фразой: Что помогает долгожителям стать долгожителями? Не здоровый образ жизни, не солярий, не фитнес, и даже, не высокая зарплата. Долгожителями становятся те, кто никогда и никуда не торопится, никогда и ни с кем не спорит, никогда и ни о чем не сожалеет. Долгожителями становятся те, у кого есть верные друзья рядом! Желаю тебе помнить об этом каждый день и прожить долгую счастливую жизнь!")
+}
+function my5() {
+    alert("Денечка! Ты-замечательный, удивительный, многогранный! С тобой всегда рядом комфортно, надежно и уютно, интересно, спокойно и просто хорошо. Твоя целеустремленность, работоспособность, настойчивость приведут тебя к прекрасным результатам, которыми мы будем гордиться и восхищаться, мы твои друзья, которые всегда будут рядом, и неважно, в какой точке мира мы находимся. Дерзай, твори, радуйся жизни и удивляй нас, любящих тебя нежно, но крепко")
+}
+    
+function my6() {
+    alert("Happy birthday, wish you much health, happiness, freedom, success in everything you do and eternal youth.")
+}
+function my7() {
+    alert("With all my heart I wish you many happy returns of the day! Let all your fond dreams, hopes and expectations come true in your new year. I wish you always to have cheerful mood, good health, strength to resist difficulties and patience, big success in all your dealings and great personal happiness! Thank you for the bright light you give me! Thank you for your being in this world! Happiness and good luck! Happy birthday!")
+}
+
+function my8() {
+    alert("Денис! Я хочу… ! Чтобы сегодняшний день стал той знаменательной точкой отсчёта, когда всё плохое отныне осталось позади. Чтобы впереди ждали только позитивные эмоции, отличные впечатления, и радостные, счастливые дни.")
+}
+function my9() {
+    alert("Чтобы каждый прожитый момент был ярким и насыщенным! ")
+}
+function my10() {
+    alert("Чтобы работа приносила удовлетворение, а гармония в душе никогда не кончалась. ")
+}
+function my11() {
+    alert("Пусть всё чего ты хочешь воплотится в твоей жизни! ")
+}
+function my12() {
+    alert("Будь всегда баловнем судьбы, сумей всегда быть в нужном месте, в нужное время. ")
+}
+
+function my13() {
+    alert("Желаю, чтобы твои прекрасные человеческие качества и таланты и дальше сияли звездами, к которым хочется тянуться. ")
+}
+function my14() {
+    alert("Желаю тебе щедрости судьбы и благосклонности удачи.  С Днём Рождения!")
+}
+function my15() {
+    alert("I wish you a long and amazing life. I wish you great health and excellent results in all your dealings. Most importantly, I wish you happiness without measure every minute in your life. Be loved, be happy, enjoy every day!")
+}
+function my16() {
+    alert("May all your dreams come true! Happy Birthday to you!")
+}
+function my17() {
+    alert("Wishing you the Best Birthday Ever! Have fun! Cheers!")
 }
